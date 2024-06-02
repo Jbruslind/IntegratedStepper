3809f514af97254135dee77ab6dd42988618a7cc
Jorian Bruslind <jbruslind@gmail.com>
2024-06-02 15:35:38 -0700

    update code for v2

--- Firmware/SimpleFOC_ESP32/src/main.cpp
+++ Firmware/SimpleFOC_ESP32/src/main.cpp
@@ -4,15 +4,30 @@
 #define CAN_TX 16
 #define CAN_RX 17
 
+#define nSLEEP 32
+#define nOFF 2
+#define DRVOFF 2
+
+#define nFAULT_1 25
+#define nFAULT_2 18
+
+#define IPROPI_1 33
+#define IPROPI_2 35
+
+#define A_p 1/3070
+#define R_IPROP 820
+
 TaskHandle_t moveMotors;
 void moveMotorsfun( void * pvParameters);
 // Stepper motor instance
 StepperMotor motor = StepperMotor(50);
 // Stepper driver instance
-StepperDriver4PWM driver = StepperDriver4PWM(26,25,33,32, 19, 23);
+StepperDriver4PWM driver = StepperDriver4PWM(27,26,5,4);
 
 MagneticSensorSPI sensor = MagneticSensorSPI(AS5048_SPI, 15);
 SPIClass* hspi = new SPIClass(HSPI); 
+
+LowsideCurrentSense current_sense = LowsideCurrentSense(A_p, R_IPROP, IPROPI_1, IPROPI_2);
 
 // CANDriver can = CANDriver(CAN_TX, CAN_RX);
 // CANCommander canCommand = CANCommander(can);
@@ -66,6 +81,21 @@ void moveMotorsfun( void * pvParameters) {
   // ##################### I2C CONFIG
   // sensor.init();
   // motor.linkSensor(&sensor);
+  
+  
+  pinMode(nSLEEP, OUTPUT);
+  pinMode(nOFF, OUTPUT);
+  pinMode(DRVOFF, OUTPUT);
+
+  pinMode(nFAULT_1, INPUT);
+  pinMode(nFAULT_2, INPUT);
+
+  digitalWrite(nOFF, HIGH);
+  digitalWrite(DRVOFF, LOW);
+
+  digitalWrite(nSLEEP, LOW);
+  delayMicroseconds(30);
+  digitalWrite(nSLEEP, HIGH);
 
   // ##################### SPI CONFIG
   sensor.init(hspi);
@@ -75,7 +105,7 @@ void moveMotorsfun( void * pvParameters) {
   // choose FOC modulation
   motor.foc_modulation = FOCModulationType::SinePWM;
   motor.torque_controller = TorqueControlType::voltage;
-  motor.controller = MotionControlType::angle;
+  motor.controller = MotionControlType::angle_openloop;
 
   // power supply voltage [V]
   driver.voltage_power_supply = 19;
@@ -105,6 +135,8 @@ void moveMotorsfun( void * pvParameters) {
   // use monitoring with serial for motor init
   // monitoring port
   
+  // current_sense.linkDriver(&driver);
+  // current_sense.init();
 
   motor.monitor_downsample = 1000;
   // initialise motor
@@ -118,9 +150,28 @@ void moveMotorsfun( void * pvParameters) {
         motor.loopFOC();
     }
 }
+int monitor_downsample = motor.monitor_downsample;
+int monitor_cnt = 0;
+int nfault_1 = 1;
+int nfault_2 = 1;
 
 void loop() {
   command.run();
   motor.monitor();
+  if( !monitor_downsample || monitor_cnt++ < (monitor_downsample-1) ) 
+  { 
+
+  }
+  else{
+    monitor_cnt = 0; 
+    nfault_1 = digitalRead(nFAULT_1);
+    nfault_2 = digitalRead(nFAULT_2);
+    // printf("NFAULT_1 : %d NFAULT_2 : %d \n", nfault_1 , nfault_2);
+    if(!nfault_1 || !nfault_2){
+      digitalWrite(nSLEEP, LOW);
+      delayMicroseconds(30);
+      digitalWrite(nSLEEP, HIGH);
+    }
+  }
   // canCommand.runWithCAN();
 }
